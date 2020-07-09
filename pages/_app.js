689d26aa7a269b687a911ac006635197ca39acd1
Jacob Zhang <walkerzjs2@gmail.com>
2020-07-10 00:06:16 +1000

    update for google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { useStore } from "../3_data/store";
 import "./_app.css";
 import Layout from "../2_containers/Layout/Layout";
 import Head from "next/head";
+import Router from "next/router";
+import * as gtag from "../2_containers/Shared/gtag";
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
-
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <div>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 //for styled components SSR support
-import Document from "next/document";
+import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
+import { GA_TRACKING_ID } from "../2_containers/Shared/gtag";
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -27,5 +27,50 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5XH934W');`,
+            }}
+          ></script>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XH934W"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
