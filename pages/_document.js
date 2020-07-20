7b8de10f3472aac4e39b94992f711bccf3ee0eae
Jacob Zhang <walkerzjs2@gmail.com>
2020-07-20 19:44:21 +1000

    re-enable google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -33,17 +33,8 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          {/* <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5XH934W');`,
-            }}
-          ></script> */}
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* <script
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
@@ -53,20 +44,13 @@ export default class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+
+            gtag('config', ${GA_TRACKING_ID}');
           `,
             }}
-          /> */}
+          />
         </Head>
         <body>
-          {/* <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XH934W"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript> */}
           <Main />
           <NextScript />
         </body>
