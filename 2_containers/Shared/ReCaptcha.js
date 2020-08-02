3a2075fb122023c0e87e78cd996494eadf1df2c9
Junshuai Zhang (Jacob) <3364486+walkerzjs@users.noreply.github.com>
2020-08-02 16:42:29 +1000

    Update ReCaptcha.js

--- 2_containers/Shared/ReCaptcha.js
+++ 2_containers/Shared/ReCaptcha.js
@@ -1,2 +1,2 @@
-export const siteKey = ;
-export const secretKey = ;
+export const siteKey = "6Ld9fLgZAAAAAFx5ouJ-kawswwyZB8uAwm51xzHI";
+export const secretKey = "6Ld9fLgZAAAAAAV1NyAGup26v8ncnHjYXgWz27k6";
