1f7a34ed9a0c9047e77f8d483e72f7fe69f1d321
Jacob <walkerzjs2@gmail.com>
2025-11-03 01:20:32 +1100

    test

--- pages/api/validateReCaptcha.js
+++ pages/api/validateReCaptcha.js
@@ -2,7 +2,7 @@ import Cors from "cors";
 import initMiddleware from "../../Shared/initMiddleWare";
 // import { secretKey } from "../../main_containers/Shared/ReCaptcha";
 const axios = require("axios");
-const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+const secretKey = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
 console.log("secretKey:", secretKey);
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
